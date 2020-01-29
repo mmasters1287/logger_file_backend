deca97c9ca189d257228f64da0a542b76306397f
Nate <nt@clain.io>
2020-01-29 14:47:10 +0300

    add json parsing

--- lib/logger_file_backend.ex
+++ lib/logger_file_backend.ex
@@ -134,9 +134,9 @@ defmodule LoggerFileBackend do
     end
   end
 
-  defp format_event(level, msg, ts, md, %{format: format, metadata: keys} = state) do
+  defp format_event(level, msg, ts, md, %{format: format, metadata: keys}) do
     IO.inspect(%{
-      "@timestamp": timestamp(ts, true),
+      "@timestamp": timestamp(ts),
       level: level,
       message: to_string(msg),
       module: md[:module],
@@ -247,59 +247,12 @@ defmodule LoggerFileBackend do
   defp prune_binary(<<>>, acc),
     do: acc
 
-  def event(level, msg, ts, md, %{fields: fields, utc_log: utc_log}) do
-    fields
-    |> format_fields(md, %{
-      "@timestamp": timestamp(ts, utc_log),
-      level: level,
-      message: to_string(msg),
-      module: md[:module],
-      function: md[:function],
-      line: md[:line]
-    })
-  end
-
-  def format_fields(fields, metadata, field_overrides) do
-    metadata
-    |> Enum.into(%{})
-    |> Map.merge(fields)
-    |> Map.merge(field_overrides)
-  end
-
-  defp timestamp(ts, utc_log) do
-    datetime(ts) <> timezone(utc_log)
+  defp timestamp(ts) do
+    datetime(ts) <> "+00:00"
   end
 
   defp datetime({{year, month, day}, {hour, min, sec, millis}}) do
     {:ok, ndt} = NaiveDateTime.new(year, month, day, hour, min, sec, {millis * 1000, 3})
     NaiveDateTime.to_iso8601(ndt)
-  end
-
-  defp timezone(true), do: "+00:00"
-  defp timezone(_), do: timezone()
-
-  defp timezone() do
-    offset = timezone_offset()
-    minute = offset |> abs() |> rem(3600) |> div(60)
-    hour = offset |> abs() |> div(3600)
-    sign(offset) <> zero_pad(hour, 2) <> ":" <> zero_pad(minute, 2)
-  end
-
-  defp timezone_offset() do
-    t_utc = :calendar.universal_time()
-    t_local = :calendar.universal_time_to_local_time(t_utc)
-
-    s_utc = :calendar.datetime_to_gregorian_seconds(t_utc)
-    s_local = :calendar.datetime_to_gregorian_seconds(t_local)
-
-    s_local - s_utc
-  end
-
-  defp sign(total) when total < 0, do: "-"
-  defp sign(_), do: "+"
-
-  defp zero_pad(val, count) do
-    num = Integer.to_string(val)
-    :binary.copy("0", count - byte_size(num)) <> num
   end
 end
